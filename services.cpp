2c2fd78d8c034430a98e7581c06c523ab822a5f8
abdelrahman99999 <abdelrahmanelsayed341@gmail.com>
2022-07-31 22:38:33 +0200

    adding services files

---
+++ services.cpp
@@ -0,0 +1,19 @@
+
+#include"services.hpp"
+#include<filesystem>
+#include <stdio.h>  /* defines FILENAME_MAX */
+// #define WINDOWS  /* uncomment this line to use it for windows.*/ 
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+std::string GetCurrentWorkingDir( void ) {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+
