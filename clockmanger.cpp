24f45cfb038acb57a214d1cc334ead7999e85e2c
abdelrahman99999 <abdelrahmanelsayed341@gmail.com>
2022-08-01 21:02:45 +0200

    change format style of the clock to be the same of format style of prayer

--- clockmanger.cpp
+++ clockmanger.cpp
@@ -9,5 +9,5 @@ std::string ClockManger::Clock_now()
     timestr.pop_back();
     auto it = timestr.find(":");
     it -= 2;
-    return timestr.substr(it, 5);//to get hours:min only
+    return '"'+timestr.substr(it, 5)+'"';//to get hours:min only
 }
